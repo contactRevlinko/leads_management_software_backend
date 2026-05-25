9072f791fcd18b16eed75cdabb9b8d258c91c806
revlinko <contact.revlinko@gmail.com>
2026-05-25 12:45:16 -0700

    cors policy issue solve

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-// main entry point
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 5000   
+    // main entry point
+    require('dotenv').config();
+    const express = require('express');
+    const app = express();
+    const cors = require('cors');
+    const mongoose = require('mongoose')
+    const PORT = process.env.PORT || 5000   
 
-//middleware
+    //middleware
 
-app.use(cors());
-app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
 
-//routes
+    app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-app.use('/api/leads', require('./routes/leads.js'));
-app.use('/api/followups', require('./routes/followups.js'));
-app.use('/api/auth' , require('./routes/auth.js'));
-//mongoDb connection
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("mongodb connected successfully"))
-.catch(err => console.log("error" , err));
+    //routes
 
-app.listen(PORT , () =>{
-    console.log(`server running on port ${PORT}`);
-});
+    app.use('/api/leads', require('./routes/leads.js'));
+    app.use('/api/followups', require('./routes/followups.js'));
+    app.use('/api/auth' , require('./routes/auth.js'));
+    //mongoDb connection
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("mongodb connected successfully"))
+    .catch(err => console.log("error" , err));
+
+    app.listen(PORT , () =>{
+        console.log(`server running on port ${PORT}`);
+    });
